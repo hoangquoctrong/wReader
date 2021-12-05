389a5c3f2065ccf9bfd0e039f5f872e68775e479
unknown <19522408@gm.uit.edu.vn>
2021-12-05 23:19:18 +0700

    added nettruyen

---
+++ wReader/lib/components/Databases/history.dart
@@ -0,0 +1,119 @@
+import 'package:wreader/components/HomeScreenComponents/detailScreen/manga_chapters.dart';
+
+final String tableHistory = 'History';
+
+class HistoryFields {
+  static final List<String> values = [
+    id,
+    mangaLink,
+    mangaTitle,
+    mangaImg,
+    mangaDesc,
+    mangaGenres,
+    mangaAuthor,
+    mangaChapter,
+    mangaChapterLink,
+    mangaChapterIndex,
+    sourceID,
+  ];
+
+  static final String id = '_id';
+  static final String mangaImg = 'mangaImg';
+  static final String mangaLink = 'mangaLink';
+  static final String mangaDesc = 'mangaDesc';
+  static final String mangaGenres = 'mangaGenres';
+  static final String mangaAuthor = 'mangaAuthor';
+  static final String mangaTitle = 'mangaTitle';
+  static final String mangaChapter = 'mangaChapter';
+  static final String mangaChapterLink = 'mangaChapterLink';
+  static final String mangaChapterIndex = 'mangaChapterIndex';
+  static final String sourceID = 'sourceID';
+}
+
+class History {
+  final int? id;
+  final String mangaLink;
+  final String mangaTitle;
+  final String mangaImg;
+  final String mangaDesc;
+  final String mangaGenres;
+  final String mangaAuthor;
+  final String mangaChapter;
+  final String mangaChapterLink;
+  final int mangaChapterIndex;
+  final int sourceID;
+
+  const History({
+    required this.mangaTitle,
+    required this.mangaLink,
+    required this.mangaImg,
+    required this.mangaDesc,
+    required this.mangaGenres,
+    required this.mangaAuthor,
+    required this.mangaChapter,
+    required this.mangaChapterLink,
+    required this.mangaChapterIndex,
+    required this.sourceID,
+    this.id,
+  });
+  History copy({
+    int? id,
+    String? mangaTitle,
+    String? mangaLink,
+    String? mangaImg,
+    String? mangaDesc,
+    String? mangaGenres,
+    String? mangaAuthor,
+    String? mangaChapter,
+    String? mangaChapterLink,
+    int? mangaChapterIndex,
+    int? sourceID,
+  }) =>
+      History(
+        mangaLink: mangaLink ?? this.mangaLink,
+        mangaImg: mangaImg ?? this.mangaImg,
+        mangaDesc: mangaDesc ?? this.mangaDesc,
+        mangaGenres: mangaGenres ?? this.mangaGenres,
+        mangaAuthor: mangaAuthor ?? this.mangaAuthor,
+        mangaTitle: mangaTitle ?? this.mangaTitle,
+        mangaChapter: mangaChapter ?? this.mangaChapter,
+        mangaChapterLink: mangaChapterLink ?? this.mangaChapterLink,
+        mangaChapterIndex: mangaChapterIndex ?? this.mangaChapterIndex,
+        id: id ?? this.id,
+        sourceID: sourceID ?? this.sourceID,
+      );
+
+  static History fromJson(Map<String, Object?> json) => History(
+        id: json[HistoryFields.id] as int,
+        mangaLink: json[HistoryFields.mangaLink] as String,
+        mangaImg: json[HistoryFields.mangaImg] as String,
+        mangaDesc: json[HistoryFields.mangaDesc] as String,
+        mangaGenres: json[HistoryFields.mangaGenres] as String,
+        mangaAuthor: json[HistoryFields.mangaAuthor] as String,
+        mangaTitle: json[HistoryFields.mangaTitle] as String,
+        mangaChapter: json[HistoryFields.mangaChapter] as String,
+        mangaChapterLink: json[HistoryFields.mangaChapterLink] as String,
+        mangaChapterIndex: json[HistoryFields.mangaChapterIndex] as int,
+        sourceID: json[HistoryFields.sourceID] as int,
+      );
+  // json[FavoriteFields.mangaLink] as String,
+  // json[FavoriteFields.mangaImg] as String,
+  // json[FavoriteFields.mangaDesc] as String,
+  // json[FavoriteFields.mangaGenres] as String,
+  // json[FavoriteFields.mangaAuthor] as String,
+  // json[FavoriteFields.id] as int);
+
+  Map<String, Object> toJson() => {
+        HistoryFields.id: id!,
+        HistoryFields.mangaAuthor: mangaAuthor,
+        HistoryFields.mangaDesc: mangaDesc,
+        HistoryFields.mangaGenres: mangaGenres,
+        HistoryFields.mangaImg: mangaImg,
+        HistoryFields.mangaLink: mangaLink,
+        HistoryFields.mangaTitle: mangaTitle,
+        HistoryFields.mangaChapter: mangaChapter,
+        HistoryFields.mangaChapterLink: mangaChapterLink,
+        HistoryFields.mangaChapterIndex: mangaChapterIndex,
+        HistoryFields.sourceID: sourceID,
+      };
+}
---
+++ wReader/lib/components/dialogs/simpleDialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SimpleDialogItem extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String text;
+  final VoidCallback onPressed;
+  const SimpleDialogItem(
+      {Key? key,
+      required this.icon,
+      required this.color,
+      required this.text,
+      required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialogOption(
+      onPressed: onPressed,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(icon, size: 36.0, color: color),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(start: 16.0),
+            child: Text(text),
+          ),
+        ],
+      ),
+    );
+  }
+}
