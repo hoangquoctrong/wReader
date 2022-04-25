a3140084207d85b6422d7b421cdabe3ce8430c33
Hoang Quoc Trong <19522408@gmail.com>
2022-04-25 14:57:18 +0700

    fix some stuff

--- wReader/lib/components/Databases/cacheInfo.dart
+++
@@ -1,9 +0,0 @@
-class CacheInfo {
-  final double size;
-  final String mangaLink;
-  final String mangaImg;
-  final String mangaTitle;
-  final int id;
-
-  CacheInfo(this.size, this.mangaLink, this.mangaImg, this.mangaTitle, this.id);
-}
